a021e9f8679d28799abc946b4d1e93a4c503db1f
Igor Vanian <igor.vanian@gmail.com>
2016-12-21 15:57:43 +0100

    Initial commit

---
+++ albums/index.android.js
@@ -0,0 +1,20 @@
+// Import a library to help create a component
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+    // <Text>Some Text</Text>
+  );
+};
+
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
